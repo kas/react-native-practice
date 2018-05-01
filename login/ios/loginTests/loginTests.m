73b4c3cdeb318d45c189ca7ff4a5abafc10ce8c3
Kenneth Schnall <kenneth.schnall@gmail.com>
2018-05-01 02:12:14 -0400

    Rename login -> Login

--- login/ios/loginTests/loginTests.m
+++ login/ios/loginTests/loginTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface loginTests : XCTestCase
+@interface LoginTests : XCTestCase
 
 @end
 
-@implementation loginTests
+@implementation LoginTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
