f2905866b270ac105028b6764d818e8cb9bdeba9
Kenneth Schnall <kenneth.schnall@gmail.com>
2018-05-02 21:08:53 -0400

    Change error to 0

--- Login/.eslintrc.js
+++ Login/.eslintrc.js
@@ -5,6 +5,6 @@ module.exports = {
     'max-len': 1,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/no-string-refs': 1,
-    'react/prop-types': ['error', { ignore: ['navigation'] }],
+    'react/prop-types': [0, { ignore: ['navigation'] }],
   },
 };
